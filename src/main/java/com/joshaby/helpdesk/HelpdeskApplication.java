14012cbf66129146ebe4193cf08c76e9ae19a8f2
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2026-07-22 17:18:58 -0300

    chore: inicializa o projeto

---
+++ src/main/java/com/joshaby/helpdesk/HelpdeskApplication.java
@@ -0,0 +1,12 @@
+package com.joshaby.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+}
